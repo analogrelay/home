c487608b09b6abafb7f042f5f1c80d726d075beb
Andrew Stanton-Nurse <git@analogrelay.net>
2022-12-23 13:22:47 -0800

    nix, inventory, ansible

--- infra/resources/dns.ts
+++ infra/resources/dns.ts
@@ -1,24 +1,50 @@
+import * as path from "path";
+import * as fs from "fs";
 import * as pulumi from "@pulumi/pulumi";
 import { resources, network } from "@pulumi/azure-native";
 
-import { meteorIp, meteorNic } from "./compute";
+// We read the Ansible Inventory to get hostname mappings
+import * as yaml from "js-yaml";
+
+const inventoryPath = path.resolve(__dirname, "../../inventory.yaml");
+const inventory: any = yaml.load(fs.readFileSync(inventoryPath, 'utf-8'));
+
+const inventoryHosts: { [key: string]: string[] } = {};
+
+function processInventoryGroup(root: any) {
+    if (typeof root.hosts === "object") {
+        for (const hostName in root.hosts) {
+            if (hostName.endsWith(".home.analogrelay.net")) {
+                const shortName = hostName.split(".")[0];
+                const hostObj = root.hosts[hostName];
+                let ips = inventoryHosts[shortName] || [];
+                inventoryHosts[shortName] = ips;
+                if (hostObj.ansible_host) {
+                    ips.push(hostObj.ansible_host);
+                }
+            }
+        }
+    }
+
+    if (typeof root.children === "object") {
+        for (const childGroupName in root.children) {
+            processInventoryGroup(root.children[childGroupName])
+        }
+    }
+}
+processInventoryGroup(inventory.all);
 
 const dnsResourceGroup = new resources.ResourceGroup("analogcloud-dns", {
     resourceGroupName: "analogcloud-dns",
     location: "WestUS3",
 });
 
+// Old nodes
 const aRecords: {name: string, ips: pulumi.Input<pulumi.Input<string>[]> }[] = [
-    { name: "biggs.home", ips: ["192.168.2.5"] },
-    { name: "reno.home", ips: ["192.168.2.6"] },
-
-    // Old nodes
     { name: "gaia.local", ips: ["192.168.1.1"] },
     { name: "wedge.local", ips: ["192.168.1.102"] },
     { name: "barret.local", ips: ["192.168.1.103"] },
     { name: "cid.local", ips: ["192.168.1.106"] },
-    { name: "jessie.local", ips: ["192.168.1.108"] },
-    { name: "tifa.local", ips: ["192.168.1.109"] },
     { name: "k8s.local", ips: ["192.168.1.3"] },
     { name: "traefik.local", ips: ["192.168.2.91"] },
 ];
@@ -74,6 +100,19 @@ aRecords.forEach(r => {
         ttl: 3600,
         recordType: "A",
         aRecords: pulumi.output(r.ips).apply(ips => ips.map(ip => {
+            return { ipv4Address: ip };
+        })),
+    });
+});
+
+Object.keys(inventoryHosts).forEach(n => {
+    new network.RecordSet(`rs-analogrelay.net-a-inv-${n}`, {
+        resourceGroupName: dnsResourceGroup.name,
+        zoneName: analogrelayZone.name,
+        relativeRecordSetName: `${n}.home`,
+        ttl: 3600,
+        recordType: "A",
+        aRecords: pulumi.output(inventoryHosts[n]).apply(ips => ips.map(ip => {
             return { ipv4Address: ip };
         })),
     });
--- shell.nix
+++ shell.nix
@@ -2,5 +2,7 @@ with (import <nixpkgs> {});
 mkShell {
     buildInputs = [
         ansible
+        nodejs
+        pulumi-bin
     ];
 }
